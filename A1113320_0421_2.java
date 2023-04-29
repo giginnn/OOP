2c4bd282e034412744b421018738681d5ef5f1e6
giginnn <125638961+giginnn@users.noreply.github.com>
2023-04-29 19:22:46 +0800

    Add files via upload

---
+++ A1113320_0421_2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class A1113320_0421_2{
+	public static void main(String[]argv){
+		System.out.println("請輸入出生西元年/月/日(月/日/西元年)");
+		String birth;
+		Scanner bd=new Scanner(System.in);
+		birth=bd.next();
+		System.out.println("生日："+birth);
+	}
+}
